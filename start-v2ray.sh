8c5fa049189c3be2789ffb7b4889421f1fc4dc53
kaiwegtminasrg <127659583+kaiwegtminasrg@users.noreply.github.com>
2023-05-10 01:31:35 +0000

    Add files via upload

---
+++ start-v2ray.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# author: gfw-breaker
+
+# start containers
+docker-compose up -d
+
+# set firewall
+systemctl stop firewalld 
+systemctl disable firewalld 
+
+# show attributes
+nic=$(ip link | egrep ' ens| eth| enp' | cut -d':' -f2)
+ip=$(ip addr show $nic | grep 'inet ' | awk '{print $2}' | cut -d'/' -f1)
+uuid=$(cat data/v2ray/config.json | grep '"id"' | cut -d'"' -f4)
+path=$(cat data/nginx/conf.d/v2ray.conf | grep location | grep -v '/ ' | awk '{print $2}')
+
+echo
+echo "v2ray客户端参数："
+echo -e "addr:\t$ip"
+echo -e "id:\t$uuid"
+echo -e "path:\t$path"
+echo
+
